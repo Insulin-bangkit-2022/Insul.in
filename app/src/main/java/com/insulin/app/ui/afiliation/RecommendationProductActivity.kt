b6c17e03457fb6a20ea842dfc98af8ebd612573e
apriantoa917 <apriantoa917@gmail.com>
2022-05-21 06:01:41 +0700

    add new category to RecommendationProduct -> vitamin & suplemens, add filter to jump each category product position

--- app/src/main/java/com/insulin/app/ui/afiliation/RecommendationProductActivity.kt
+++ app/src/main/java/com/insulin/app/ui/afiliation/RecommendationProductActivity.kt
@@ -1,32 +1,35 @@
 package com.insulin.app.ui.afiliation
 
-import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import android.widget.ProgressBar
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.Query
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
+import com.insulin.app.R
 import com.insulin.app.adapter.article.AffiliationProductAdapterHorizontal
 import com.insulin.app.adapter.article.AffiliationProductAdapterVertical
 import com.insulin.app.data.model.AffiliationProduct
 import com.insulin.app.databinding.ActivityRecomendationProductBinding
-import com.insulin.app.utils.Helper
+
 
 class RecommendationProductActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRecomendationProductBinding
     private val list0: ArrayList<AffiliationProduct> = ArrayList() // alat test
     private val list1: ArrayList<AffiliationProduct> = ArrayList() // groceries
-    private val list2: ArrayList<AffiliationProduct> = ArrayList() // snack
+    private val list2: ArrayList<AffiliationProduct> = ArrayList() // vitamin
+    private val list3: ArrayList<AffiliationProduct> = ArrayList() // snack
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,7 +45,7 @@ class RecommendationProductActivity : AppCompatActivity() {
         }
     }
 
-    fun loadAffiliationProductData(
+    private fun loadAffiliationProductData(
     ) {
         binding.progressBarAffiliation.isVisible = true
         val TAG = "FIREBASE"
@@ -52,13 +55,15 @@ class RecommendationProductActivity : AppCompatActivity() {
                     list0.clear()
                     list1.clear()
                     list2.clear()
+                    list3.clear()
                     for (article in dataSnapshot.children) {
                         val data = article.getValue<AffiliationProduct>()
                         data?.let {
                             when (it.product_category) {
                                 0 -> list0.add(it)
                                 1 -> list1.add(it)
-                                2 -> list2.add(it)
+                                2 -> list3.add(it)
+                                3 -> list2.add(it)
                                 else -> {}
                             }
                         }
@@ -85,6 +90,13 @@ class RecommendationProductActivity : AppCompatActivity() {
                         it.setHasFixedSize(true)
                         it.layoutManager = LinearLayoutManager(this@RecommendationProductActivity)
                         it.isNestedScrollingEnabled = false
+                        list3.reverse()
+                        it.adapter = AffiliationProductAdapterVertical(list3)
+                    }
+                    binding.rvAffiliation3.let {
+                        it.setHasFixedSize(true)
+                        it.layoutManager = LinearLayoutManager(this@RecommendationProductActivity)
+                        it.isNestedScrollingEnabled = false
                         list2.reverse()
                         it.adapter = AffiliationProductAdapterVertical(list2)
                     }
@@ -97,5 +109,42 @@ class RecommendationProductActivity : AppCompatActivity() {
                     // ...
                 }
             })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_product, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.navigation_filter -> {
+                val alertdialogbuilder = AlertDialog.Builder(this@RecommendationProductActivity)
+                alertdialogbuilder.setTitle("Kategori Produk")
+                val items = arrayOf(
+                    "🔬 Test Diabetes",
+                    "🌾 Kebutuhan Harian",
+                    "💊 Suplemen & Vitamin",
+                    "🍔 Snack Rendah Gula"
+                )
+                alertdialogbuilder.setItems(
+                    items
+                ) { _, index ->
+                    when (index) {
+                        0 -> scrollToView(binding.labelC0)
+                        1 -> scrollToView(binding.labelC1)
+                        2 -> scrollToView(binding.labelC3)
+                        3 -> scrollToView(binding.labelC2)
+                    }
+                }
+                alertdialogbuilder.create().show()
+            }
+        }
+        return true
+    }
+
+    private fun scrollToView(view: View) {
+        val positionY = view.top - view.height
+        binding.nestedScrollView.smoothScrollTo(binding.root.left, positionY)
     }
 }
