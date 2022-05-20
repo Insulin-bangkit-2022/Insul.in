3e499bd4b593434a7bbd1ffa877df4fd96f1d1d2
apriantoa917 <apriantoa917@gmail.com>
2022-05-20 08:12:02 +0700

    serialize kotlin models with firebase database notation (@PropertyName(...))

--- app/src/main/java/com/insulin/app/data/model/Article.kt
+++ app/src/main/java/com/insulin/app/data/model/Article.kt
@@ -1,9 +1,18 @@
 package com.insulin.app.data.model
 
+import com.google.firebase.database.PropertyName
+
 data class Article (
-    var id: String = "",
+
+    @PropertyName("image_url")
     var image_url: String = "",
+
+    @PropertyName("image_url")
     var title: String = "",
+
+    @PropertyName("provider_name")
     var provider_name: String = "",
+
+    @PropertyName("url")
     var url: String = "",
 )
--- app/src/main/java/com/insulin/app/data/model/Detection.kt
+++ app/src/main/java/com/insulin/app/data/model/Detection.kt
@@ -1,19 +1,51 @@
 package com.insulin.app.data.model
 
+import com.google.firebase.database.PropertyName
+import com.insulin.app.utils.Constanta
+
 data class Detection(
+    @PropertyName("diabetes")
     var isDiabetes: Boolean = false,
+
+    @PropertyName("detectionTime")
     var detectionTime: String = "",
+
+    @PropertyName("polyuria")
     var isPolyuria: Boolean = false,
+
+    @PropertyName("polydipsia")
     var isPolydipsia: Boolean = false,
+
+    @PropertyName("weightLoss")
     var isWeightLoss: Boolean = false,
+
+    @PropertyName("weakness")
     var isWeakness: Boolean = false,
+
+    @PropertyName("polyphagia")
     var isPolyphagia: Boolean = false,
+
+    @PropertyName("genitalThrus")
     var isGenitalThrus: Boolean = false,
+
+    @PropertyName("itching")
     var isItching: Boolean = false,
+
+    @PropertyName("irritability")
     var isIrritability: Boolean = false,
+
+    @PropertyName("delayedHealing")
     var isDelayedHealing: Boolean = false,
+
+    @PropertyName("partialParesis")
     var isPartialParesis: Boolean = false,
+
+    @PropertyName("muscleStiffness")
     var isMuscleStiffness: Boolean = false,
+
+    @PropertyName("alopecia")
     var isAlopecia: Boolean = false,
+
+    @PropertyName("obesity")
     var isObesity: Boolean = false
 )
